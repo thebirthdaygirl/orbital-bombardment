6edee51f4c44dd599a2f8adaefcb8d4958381428
Kareena Rogers <kareenarr@gmail.com>
2024-09-24 21:07:12 +0530

    updated tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}', // Include all JS/TS/JSX/TSX files in the src folder
+    './public/index.html'         // Include index.html in the public folder
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
