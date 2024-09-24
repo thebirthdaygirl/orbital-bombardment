eda7a5678a1fbed377b6f3472235457d49f48eea
Kareena Rogers <kareenarr@gmail.com>
2024-09-24 23:13:26 +0530

    Update App.js

--- src/core/App.js
+++ src/core/App.js
@@ -1,9 +1,10 @@
-import Game from './Game';
+import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <Game />
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Hello from Vercel!</h1>
+      <p>If you can see this, your React app is working.</p>
     </div>
   );
 }
